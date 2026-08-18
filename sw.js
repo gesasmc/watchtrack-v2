ce1aa85234a0e57b744d5e4b2c084ca680ec0264
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-08-18 20:18:53 +0200

    Refresh PWA cache for family key sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='watchtrack-v2.5-family-sync-1';
+const CACHE='watchtrack-v2.5.1-family-key-1';
 const SHELL=['./','./index.html','./styles.css','./features.css','./custom.css','./v24.css','./sync.css','./app.js','./features.js','./custom.js','./v24.js','./sync.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
