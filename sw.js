9fc609444a5afd9564bd4f6b19046c365406db49
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-08-18 22:15:55 +0200

    Refresh cache for v2.8.1 notifications

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='watchtrack-v2.8-for-you';
+const CACHE='watchtrack-v2.8.1-notifications';
 const SHELL=['./','./index.html','./styles.css','./features.css','./custom.css','./v24.css','./sync.css','./notifications.css','./next-up.css','./app.js','./features.js','./custom.js','./v24.js','./sync.js','./series-first.js','./stable-progress.js','./notifications.js','./next-up.js','./recommendations.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
