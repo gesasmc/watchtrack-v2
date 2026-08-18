3a08e2538dabac97aa64e72d72d21610fffbae05
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-08-18 10:47:43 +0200

    Refresh PWA cache for new WatchTrack features

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='watchtrack-v2';
-const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='watchtrack-v2.1-features-1';
+const SHELL=['./','./index.html','./styles.css','./features.css','./app.js','./features.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
