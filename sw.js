9e54269c8ffa209d5de8be6c5354e578e19b35c0
gesasmc <pz9v7b4rwr@privaterelay.appleid.com>
2026-08-18 21:11:08 +0200

    Refresh cache for simplified series library

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='watchtrack-v2.7.1-library-tabs';
+const CACHE='watchtrack-v2.7.2-series-tabs';
 const SHELL=['./','./index.html','./styles.css','./features.css','./custom.css','./v24.css','./sync.css','./notifications.css','./next-up.css','./app.js','./features.js','./custom.js','./v24.js','./sync.js','./series-first.js','./stable-progress.js','./notifications.js','./next-up.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
